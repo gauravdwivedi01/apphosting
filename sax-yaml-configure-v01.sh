1e9c7f48e63ccc728dc7daf571e3c70bbd65e577
gauravdwivedi01 <38350529+gauravdwivedi01@users.noreply.github.com>
2018-07-23 18:27:58 +0530

    - yarn

--- sax-yaml-configure-v01.sh
+++ sax-yaml-configure-v01.sh
@@ -257,7 +257,7 @@ echo "RM_HA_HOSTS : $RM_HA_HOSTS"
 ## replace SPARK2 configurations at env-config.yaml
 
 sed -ri 's|^(\s*)(home\s*:\s*/usr/hdp/2.4.2.0-258/spark\s*$)|\1home: '"$SPARK_HOME"'|' $CLOUD_TEMPLATE_YAML_FILE
-sed -ri 's|^(\s*)(cluster.manager\s*:\s*"standalone"\s*$)|\1cluster.manager: '"yarn"'|' $CLOUD_TEMPLATE_YAML_FILE
+#sed -ri 's|^(\s*)(cluster.manager\s*:\s*"standalone"\s*$)|\1cluster.manager: '"yarn"'|' $CLOUD_TEMPLATE_YAML_FILE
 sed -ri 's|^(\s*)(history.server\s*:\s*"localhost:18080"\s*$)|\1history.server: '"$SPARK_HISTORY_SERVER_URI"'|' $CLOUD_TEMPLATE_YAML_FILE
 sed -ri 's|^(\s*)(resource.manager.host\s*:\s*localhost\s*$)|\1resource.manager.host: '"$PRIMARY_HEAD_NODE"'|' $CLOUD_TEMPLATE_YAML_FILE
 sed -ri 's|^(\s*)(resource.manager.isHA\s*:\s*"false"s*$)|\1resource.manager.isHA: 'true'|' $CLOUD_TEMPLATE_YAML_FILE
